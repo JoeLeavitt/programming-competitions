b5e1fd7255e306e03442cf38b96ad33166341cc1
Joe <joeleav1@gmail.com>
2016-02-03 16:50:00 -0500

    week 3

--- src/news.java
+++ src/news.java
@@ -1,5 +1,55 @@
 /**
  * Created by JL on 1/29/16.
  */
+
+import java.util.*;
+
 public class news {
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+
+        int testCases = stdin.nextInt();
+
+        for (int i = 0; i < testCases; i++) {
+            int employees = stdin.nextInt();
+
+            ArrayList[] tree = new ArrayList[employees];
+
+            for (int j = 0; j < employees; j++) {
+                tree[j] = new ArrayList<Integer>();
+            }
+
+            for (int j = 1; j < employees; j++) {
+                int parent = stdin.nextInt();
+
+                tree[parent].add(j);
+            }
+
+            int solution = solve(tree, 0);
+
+            System.out.println(solution);
+        }
+    }
+
+    public static int solve(ArrayList[] tree, int index) {
+        ArrayList<Integer> values = new ArrayList<>();
+        int minTime = 0;
+
+        for(int i = 0; i < tree[index].size(); i++) {
+           values.add(solve(tree, (Integer) tree[index].get(i)));
+        }
+
+        Collections.sort(values, Collections.reverseOrder());
+
+        for (int i = 0; i < values.size(); i++) {
+            int curr = values.get(i);
+
+            if (curr + i + 1 > minTime) {
+                minTime = curr + i + 1;
+            }
+        }
+
+        return minTime;
+    }
 }
